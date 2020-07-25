473ce1d7c417b730c3aba51c9e6d10e5ddf997a1
tkdguswjs1212 <tkdguswjs1212@naver.com>
2020-07-25 13:38:29 +0900

    스쿨걸수정
    스쿨걸행동패턴수정

--- RiverCityGirls/SchoolGirl.cpp
+++ RiverCityGirls/SchoolGirl.cpp
@@ -71,11 +71,16 @@ void SchoolGirl::update()
 		moveDir = Vector3::normalize(&moveDir);
 		moveDir = moveDir * 2;
 		moveDir.y += _gravity;
-
+		
 		float lastY = _position.y;
+		float lastX = _position.x;		
 		_enemyManager->moveEnemy(this, moveDir);
 		float currY = _position.y;
-
+		float currX = _position.x;
+		if (lastX == currX && _elapsedTime > 3.0 && distanceFromPlayer > 90 && _state == ENEMY_STATE::WALK)
+		{
+			_position.z -= 4;
+		}
 		if (lastY != currY) // 떨어짐
 		{
 			setState(ENEMY_STATE::JUMP, _direction);
@@ -86,22 +91,22 @@ void SchoolGirl::update()
 			if (distanceFromPlayer <= 100) // 근접 시 공격
 			{
 				setState(ENEMY_STATE::ATTACK, _direction);
+				if (_state != ENEMY_STATE::ATTACK && _hitType == ATTACK_TYPE::HIT1)
+				{
+					setState(ENEMY_STATE::GUARD, _direction);
+				}
 			}
 			else if (_elapsedTime >= 5) // 오랜 쫓음으로 인해 한번 점프해본다
 			{
-				setState(ENEMY_STATE::JUMP, _direction);
+				setState(ENEMY_STATE::SKILL, _direction);
 				_gravity = -22;
 			}			
-		}
-		if (_elapsedTime > 2 && _position.x == _position.x  && playerPos.z >= _position.z)
-		{
-			_position.z += 1;
-		}
-		else
-		{
-			_position.z -= 1;
-		}
-
+		}	
+		
+		
+		
+		
+		
 		
 	}
 	break;
@@ -118,9 +123,14 @@ void SchoolGirl::update()
 		moveDir.y += _gravity;
 
 		float lastY = _position.y;
+		float lastX = _position.x;
 		_enemyManager->moveEnemy(this, moveDir);
 		float currY = _position.y;
-
+		float currX = _position.x;
+		if (lastX == currX && _elapsedTime > 3.0 && distanceFromPlayer > 90 && _state == ENEMY_STATE::RUN)
+		{
+			_position.z -= 4;
+		}
 		if (lastY != currY) // 떨어짐
 		{
 			setState(ENEMY_STATE::JUMP, _direction);
@@ -138,14 +148,7 @@ void SchoolGirl::update()
 				_gravity = -26;
 			}
 		}
-		if (_elapsedTime > 2 && _position.x == _position.x  && playerPos.z >= _position.z)
-		{
-			_position.z += 1;
-		}
-		else
-		{
-			_position.z -= 1;
-		}
+		
 	
 	}
 	break;
@@ -194,16 +197,12 @@ void SchoolGirl::update()
 		_viewRc = FloatRect(_attackRc.left, _position.z + _attackRc.top,
 			_attackRc.right, _position.z + _attackRc.bottom);	
 
-		//enemyAttack(_attackRc, 5, ATTACK_TYPE::HIT1); 활성화되면 터짐
-
-		
+		//enemyAttack(_attackRc, 5, ATTACK_TYPE::HIT1);		
 
 		if (!_ani->isPlay()) // 공격 모션이 끝났다면
 		{
 			setState(ENEMY_STATE::IDLE, _direction);
-		}
-		
-		
+		}	
 	
 	}
 	break;
@@ -227,6 +226,55 @@ void SchoolGirl::update()
 		{
 			setState(ENEMY_STATE::IDLE, _direction);
 		}
+
+		//enemyAttack(_attackRc, 5, ATTACK_TYPE::HIT2);
+	}
+	break;
+	case ENEMY_STATE::SKILL:
+	{
+
+		_gravity += 1;
+		moveDir.x += (_direction == DIRECTION::RIGHT) ? 1 : -1;
+		moveDir.z += (playerPos.z >= _position.z + 10) ? 1 : ((playerPos.z <= _position.z - 10) ? -1 : 0);
+		moveDir = Vector3::normalize(&moveDir);
+		moveDir = moveDir * 2;
+		moveDir.y += _gravity;
+
+		float lastY = _position.y;
+		_enemyManager->moveEnemy(this, moveDir);
+		float currY = _position.y;
+
+		if (_gravity > 1 && lastY == currY) // 착지한 경우
+		{
+			_gravity = 0;
+			if (distanceFromPlayer > 400)
+			{
+				setState(ENEMY_STATE::RUN, _direction); // 플레이어에게 달려가기
+			}
+			else
+			{
+				setState(ENEMY_STATE::WALK, _direction); // 플레이어에게 걸어가기
+			}
+		}
+
+		if (_direction == DIRECTION::LEFT)
+		{
+			_attackRc = FloatRect(_position.x - 130, _position.y - 35,
+				_position.x - 20, _position.y + 20);
+		}
+		else if (_direction == DIRECTION::RIGHT)
+		{
+			_attackRc = FloatRect(_position.x + 20, _position.y - 35,
+				_position.x + 100, _position.y + 20);
+		}
+		_viewRc = FloatRect(_attackRc.left, _position.z + _attackRc.top,
+			_attackRc.right, _position.z + _attackRc.bottom);
+		if (!_ani->isPlay()) // 공격 모션이 끝났다면
+		{
+			setState(ENEMY_STATE::IDLE, _direction);
+		}
+
+		//enemyAttack(_attackRc, 10, ATTACK_TYPE::KNOCKDOWN);
 	}
 	break;
 
@@ -288,6 +336,14 @@ void SchoolGirl::update()
 	break;
 
 	case ENEMY_STATE::STUN: // 스턴 걸린 상태
+	{
+		if (_elapsedTime > 3.0f)
+		{
+			setState(ENEMY_STATE::IDLE, _direction);
+		}
+	}
+	break;
+	case ENEMY_STATE::GUARD: // 가드상태
 	{
 		if (_elapsedTime > 3.0f)
 		{
