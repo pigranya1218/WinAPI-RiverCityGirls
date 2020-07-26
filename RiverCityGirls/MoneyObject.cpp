665a11b17f3fa18a62c328a65c6a948acd44a90e
RhinoMrMoon <68267826+RhinoMrMoon@users.noreply.github.com>
2020-07-26 17:51:34 +0900

    moneyOb valueY change
    MoneyObject generate Yvalue change

--- RiverCityGirls/MoneyObject.cpp
+++ RiverCityGirls/MoneyObject.cpp
@@ -5,9 +5,8 @@ MoneyObject::MoneyObject(Vector3 pos)
 {
 	_position = pos;
 	_size = Vector3(30, 100, 30);
-	_position.y = -_size.y * 1.5;
+	_position.y = -_size.y * 2;
 	_remainTime = 10.0f;
-	_gravity = 0.8f;
 	_randomSpawn = RANDOM->getInt(5) + 1;
 
 	_state = OBJECT_STATE::IDLE;
@@ -15,26 +14,31 @@ MoneyObject::MoneyObject(Vector3 pos)
 	{
 		case 1:
 		{
+			_gravity = 8.0f;
 			_img = IMAGE_MANAGER->findImage("OBJECT_MOVE_COIN_SILVER");
 		}
 		break;
 		case 2:
 		{
+			_gravity = 8.0f;
 			_img = IMAGE_MANAGER->findImage("OBJECT_MOVE_COIN_GOLD");
 		}
 		break;
 		case 3:
 		{
+			_gravity = 0.8f;
 			_img = IMAGE_MANAGER->findImage("OBJECT_MOVE_MONEY_BLUE");
 		}
 		break;
 		case 4:
 		{
+			_gravity = 0.8f;
 			_img = IMAGE_MANAGER->findImage("OBJECT_MOVE_MONEY_PURPLE");
 		}
 		break;
 		case 5:
 		{
+			_gravity = 0.8f;
 			_img = IMAGE_MANAGER->findImage("OBJECT_MOVE_MONEY_GREEN");
 		}
 		break;
@@ -54,6 +58,7 @@ void MoneyObject::update()
 		_ani->stop();
 		_gravity = 0;
 	}
+
 	_remainTime -= TIME_MANAGER->getElapsedTime();
 
 	if (_remainTime <= 0)
@@ -106,31 +111,31 @@ void MoneyObject::eatEffect(Player * player)
 		case 1:
 		{
 				_state = OBJECT_STATE::INACTIVE;
-				player->setMoney(player->getMoney() + 5);
+				player->setMoney(player->getMoney() + 5.15f);
 		}
 		break;
 		case 2:
 		{
 				_state = OBJECT_STATE::INACTIVE;
-				player->setMoney(player->getMoney() + 10);
+				player->setMoney(player->getMoney() + 10.25f);
 		}
 		break;
 		case 3:
 		{
 				_state = OBJECT_STATE::INACTIVE;
-				player->setMoney(player->getMoney() + 15);
+				player->setMoney(player->getMoney() + 15.00f);
 		}
 		break;
 		case 4:
 		{
 			_state = OBJECT_STATE::INACTIVE;
-			player->setMoney(player->getMoney() + 20);
+			player->setMoney(player->getMoney() + 20.15f);
 		}
 		break;
 		case 5:
 		{
 			_state = OBJECT_STATE::INACTIVE;
-			player->setMoney(player->getMoney() + 30);
+			player->setMoney(player->getMoney() + 30.25f);
 		}
 		break;
 	}
